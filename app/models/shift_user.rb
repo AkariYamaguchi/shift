2889a8cd89951ea1d32de83468e5bb06dc0122fd
AkariYamaguchi <akr03@gmail.com>
2020-01-11 12:16:05 +0900

    fixed seed

--- app/models/shift_user.rb
+++ app/models/shift_user.rb
@@ -1,7 +1,18 @@
+# frozen_string_literal: true
+
 class ShiftUser < ApplicationRecord
-    enum work_type:{
-    "午前": 1,
-    "午後": 2,
-    "1日": 3,
+  enum work_type: {
+    am: 1,
+    pm: 2,
+    all_day: 3
   }
+  # 英語から日本語
+  WORK_TYPE = {
+    am: '午前',
+    pm: '午後',
+    all_day: '1日'
+  }.freeze
+  def display_work_type
+    WORK_TYPE[work_type.try(:to_sym)]
+  end
 end
