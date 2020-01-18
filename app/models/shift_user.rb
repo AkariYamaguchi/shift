1a0ca47e9aeaa4cb59cae44d91fdb541a7c6fe45
Yusuke Yasuo <yasuo.y@dely.jp>
2020-01-18 15:01:16 +0900

    シフト登録実装

--- app/models/shift_user.rb
+++ app/models/shift_user.rb
@@ -2,16 +2,19 @@
 
 class ShiftUser < ApplicationRecord
   enum work_type: {
+    holiday: 0,
     am: 1,
     pm: 2,
     all_day: 3
   }
   # 英語から日本語
   WORK_TYPE = {
+    holiday: '-',
     am: '午前',
     pm: '午後',
     all_day: '1日'
   }.freeze
+
   def display_work_type
     WORK_TYPE[work_type.try(:to_sym)]
   end
