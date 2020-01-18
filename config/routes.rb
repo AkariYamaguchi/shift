754009ef87c302b07b09a0a568349afbc837fae7
AkariYamaguchi <akr03@gmail.com>
2020-01-18 14:00:48 +0900

    setting profile page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   resources :shift_users
   resources :events
   devise_for :users
+    get "users" => "user#index"
+    get "users/:id" => "user#show"
+    post "users/:id/update" =>"user#edit"
+
   root 'home#top'
 
 end
