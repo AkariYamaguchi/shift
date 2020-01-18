ec23c859501fb2a71da02b922e89f9ff5a382bfd
AkariYamaguchi <akr03@gmail.com>
2020-01-18 12:17:22 +0900

    fixed new

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   resources :events
   devise_for :users
   root 'home#top'
-
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
 end
