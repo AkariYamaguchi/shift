a786f49b21bf32fe7da02f8111f88225221d7928
AkariYamaguchi <akr03@gmail.com>
2020-01-20 17:44:30 +0900

    devise　editバク修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,18 @@ Rails.application.routes.draw do
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
+  root 'home#top'
   resources :shift_users
   resources :events
-  devise_for :users
+
+
+  #registrations_controller.rbを使える設定
+  devise_for :users, controllers: { registrations: 'users/registrations' }
+
     get "users" => "user#index"
     get "users/show" => "user#show"
     get "users/:id/edit" => "user#edit"
     post "users/:id/update" =>"user#edit"
 
-  root 'home#top'
 
 end
