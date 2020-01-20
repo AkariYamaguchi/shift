0082967e9991b1fc3bba3e22a3b8c34d28a0fa88
AkariYamaguchi <akr03@gmail.com>
2020-01-20 14:42:26 +0900

    サインアップ時に名前登録

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   resources :shift_users
   resources :events
   devise_for :users
