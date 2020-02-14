26426255e08baf6abe204a1d6cc5855d2a365248
AkariYamaguchi <akr03@gmail.com>
2020-02-14 21:54:30 +0900

    不要なカラム削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_10_114128) do
+ActiveRecord::Schema.define(version: 2020_02_14_125239) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,7 +35,6 @@ ActiveRecord::Schema.define(version: 2020_02_10_114128) do
     t.integer "work_type", limit: 2
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "apply_status", default: "applying", null: false
     t.integer "status", default: 0, null: false
   end
 
@@ -55,6 +54,7 @@ ActiveRecord::Schema.define(version: 2020_02_10_114128) do
     t.datetime "updated_at", null: false
     t.string "name"
     t.text "profile"
+    t.boolean "admin_flag", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
