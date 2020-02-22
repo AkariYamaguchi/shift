2678f50c55838e46918bd81a55bb6b82da47ef5c
AKARI YAMAGUCHI <akr03xxx@gmail.com>
2020-02-22 17:43:55 +0900

    rubocop

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
@@ -5,12 +7,11 @@ Rails.application.routes.draw do
   root 'shift_users#index'
   resources :shift_users
 
-  #registrations_controller.rbを使える設定
-  devise_for :users, controllers: { registrations: 'users/registrations',omniauth_callbacks: 'users/omniauth_callbacks' }
+  # registrations_controller.rbを使える設定
+  devise_for :users, controllers: { registrations: 'users/registrations', omniauth_callbacks: 'users/omniauth_callbacks' }
 
-    get "users" => "user#index"
-    get "users/show" => "user#show"
-    get "users/:id/edit" => "user#edit"
-    post "users/:id/update" =>"user#edit"
-
+  get 'users' => 'user#index'
+  get 'users/show' => 'user#show'
+  get 'users/:id/edit' => 'user#edit'
+  post 'users/:id/update' => 'user#edit'
 end
