dd3b1339f25b811dd343862d76bb900d9e7d7398
Yusuke Yasuo <yasuo.y@dely.jp>
2020-02-24 12:47:03 +0900

    fix: migrationファイルの構文エラー解消

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2020_02_18_115255) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +34,11 @@ ActiveRecord::Schema.define(version: 2020_02_18_115255) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "shift_applications", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "shift_users", force: :cascade do |t|
