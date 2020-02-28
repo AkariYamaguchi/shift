d49f1524641154b742ed405269e5fd47f7995d92
AKARI YAMAGUCHI <akr03xxx@gmail.com>
2020-02-28 12:38:45 +0900

    add: commentにuser_id追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_24_060220) do
+ActiveRecord::Schema.define(version: 2020_02_28_033546) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2020_02_24_060220) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "shift_users", force: :cascade do |t|
