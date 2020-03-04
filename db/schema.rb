b7f70e82c9d8df53db71d583fae4bdbf1025ac8f
AKARI YAMAGUCHI <akr03xxx@gmail.com>
2020-03-04 14:56:04 +0900

    likeテーブル追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2020_02_18_115255) do
-
+ActiveRecord::Schema.define(version: 2020_03_04_055325) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +34,20 @@ ActiveRecord::Schema.define(version: 2020_02_18_115255) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.integer "comment_id"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "shift_users", force: :cascade do |t|
