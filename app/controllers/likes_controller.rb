1e18b3a3db17a8ad729b757854a9e76136ee09db
AKARI YAMAGUCHI <akr03xxx@gmail.com>
2020-03-07 16:23:08 +0900

    find_or_create_byに変更

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,7 +1,7 @@
 class LikesController < ApplicationController
   def create
     #comment_idの取得
-    Like.create(comment_id: params[:comment_id], user_id: current_user.id)
+    Like.find_or_create_by(comment_id: params[:comment_id], user_id: current_user.id)
     #リダイレクトの設定
     redirect_to comments_path
   end
