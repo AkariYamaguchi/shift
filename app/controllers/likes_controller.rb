27074a39f5a47f2f48417825a649a8b12854da3b
AKARI YAMAGUCHI <akr03xxx@gmail.com>
2020-03-07 16:19:57 +0900

    いいね実装

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,3 +1,8 @@
-def create
-
+class LikesController < ApplicationController
+  def create
+    #comment_idの取得
+    Like.create(comment_id: params[:comment_id], user_id: current_user.id)
+    #リダイレクトの設定
+    redirect_to comments_path
+  end
 end
